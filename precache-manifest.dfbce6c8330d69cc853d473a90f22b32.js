5990e3aff9fdd175fcd07bb06eef0da2b74ecfaa
dabeloper <dabeloper@gmail.com>
2020-02-26 16:59:35 -0500

    Updates

---
+++ precache-manifest.dfbce6c8330d69cc853d473a90f22b32.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "062f6fbe06f5cc392758de7200ecb949",
+    "url": "/portafolio/index.html"
+  },
+  {
+    "revision": "76b0be6948a47e42263b",
+    "url": "/portafolio/static/css/main.2e215c5d.chunk.css"
+  },
+  {
+    "revision": "f5a3b64fdcd9c9497b9c",
+    "url": "/portafolio/static/js/2.00b92b10.chunk.js"
+  },
+  {
+    "revision": "7f84d11092c10d3a43893d78f68c23cb",
+    "url": "/portafolio/static/js/2.00b92b10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76b0be6948a47e42263b",
+    "url": "/portafolio/static/js/main.9cded18d.chunk.js"
+  },
+  {
+    "revision": "86a82c0d1a8a2497e737",
+    "url": "/portafolio/static/js/runtime-main.7bece6c5.js"
+  },
+  {
+    "revision": "32400f4e08932a94d8bfd2422702c446",
+    "url": "/portafolio/static/media/fontawesome-webfont.32400f4e.eot"
+  },
+  {
+    "revision": "a35720c2fed2c7f043bc7e4ffb45e073",
+    "url": "/portafolio/static/media/fontawesome-webfont.a35720c2.woff"
+  },
+  {
+    "revision": "a3de2170e4e9df77161ea5d3f31b2668",
+    "url": "/portafolio/static/media/fontawesome-webfont.a3de2170.ttf"
+  },
+  {
+    "revision": "db812d8a70a4e88e888744c1c9a27e89",
+    "url": "/portafolio/static/media/fontawesome-webfont.db812d8a.woff2"
+  },
+  {
+    "revision": "f775f9cca88e21d45bebe185b27c0e5b",
+    "url": "/portafolio/static/media/fontawesome-webfont.f775f9cc.svg"
+  }
+]);
