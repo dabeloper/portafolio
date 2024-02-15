444e4f0b629417ae5424f7965912c95ecd889fbe
David B <dabeloper+github@gmail.com>
2024-02-14 20:04:19 -0500

    [2024] DABELOPER
    Site updated

---
+++ README.md
@@ -0,0 +1,5 @@
+# Portafolio
+
+This project was created with Love using Angular version 17.0.10.
+
+# DABELOPER
---
+++ chunk-HO7XNA7P.js
@@ -0,0 +1,1 @@
+import{N as a,O as b,P as c}from"./chunk-OWTB6FFY.js";export{a as getPerformance,b as initializePerformance,c as trace};
